ee54b4cc15e5cd030549119dc8bb20e825efff33
Gregor MacDougall <gregor@freerunningtechnologies.com>
2016-08-31 09:23:58 -0400

    Implemented verse 1 back to zero!

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -2,17 +2,28 @@ class Bottles
   def verse(number)
     <<-VERSE
 #{n_bottles(number)} on the wall, #{n_bottles(number)}.
-Take one down and pass it around, #{n_bottles(number - 1)} on the wall.
+Take #{thing_name(number)} down and pass it around, #{n_bottles(number - 1)} on the wall.
 VERSE
   end
 
   private
 
-  def n_bottles(number)
+  def thing_name(number)
     if number == 1
-      "#{number} bottle of beer"
+      'it'
     else
-      "#{number} bottles of beer"
+      'one'
     end
+  end
+
+  def n_bottles(number)
+    case number
+    when 0
+      "no more bottles"
+    when 1
+      "1 bottle"
+    else
+      "#{number} bottles"
+    end + " of beer"
   end
 end
